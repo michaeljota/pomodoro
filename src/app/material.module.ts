cff6966ca5f206e03d9102c29af143170a09e3a5
Michael De Abreu <michaeljota@outlook.com>
2017-07-17 08:09:06 -0400

    feat(material): add mdButtonModule to material module

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import {
+  MdButtonModule,
 } from '@angular/material';
 
 @NgModule({
   exports: [
+    MdButtonModule,
   ],
   imports: [
+    MdButtonModule,
   ],
 })
 export class MaterialModule {

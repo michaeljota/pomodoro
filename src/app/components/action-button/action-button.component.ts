4d712808b992db0ee835b877418f6ca3b9f873fa
Michael De Abreu <michaeljota@outlook.com>
2017-07-20 19:59:05 -0400

    feat(action-button): update to manage timer

--- src/app/components/action-button/action-button.component.ts
+++ src/app/components/action-button/action-button.component.ts
@@ -9,7 +9,7 @@ import { Subscription } from 'rxjs/Rx';
 
 import { IAppState } from 'app/app.reducer';
 
-import { CicleDispacher } from 'app/actions';
+import { CicleDispacher, TimerDispacher } from 'app/actions';
 import { Cicle } from 'app/models';
 import { CicleState } from 'app/reducers';
 
@@ -31,6 +31,7 @@ export class ActionButtonComponent implements OnInit, OnDestroy {
   public text: string;
   private _currentCicle: Cicle;
   private _cicleSubscription: Subscription;
+  private _tickerId: number;
 
   constructor(
     private readonly _store: Store<IAppState>,
@@ -51,7 +52,12 @@ export class ActionButtonComponent implements OnInit, OnDestroy {
     switch (this._currentCicle) {
       case Cicle.STOP:
       case Cicle.LONG_BREAK:
-      case Cicle.SHORT_BREAK:
+      case Cicle.SHORT_BREAK: {
+        this._store.dispatch(new TimerDispacher().set(25 * 60));
+        this._store.dispatch(new CicleDispacher().start());
+        this._tickerId = window.setInterval(this.tick, 1000);
+        break;
+      }
       case Cicle.PAUSE:
         this._store.dispatch(new CicleDispacher().start());
         break;
@@ -79,6 +85,13 @@ export class ActionButtonComponent implements OnInit, OnDestroy {
       default:
         this.text = 'PAUSE';
     }
+  }
+
+  private tick = (): void => {
+    if (this._currentCicle === Cicle.PAUSE) {
+      return;
+    }
+    this._store.dispatch(new TimerDispacher().subtract(1));
   }
 
 }

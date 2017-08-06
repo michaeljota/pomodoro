f118d8dd0e4b0c056a050f8e3d3c9a151ce934ff
Michael De Abreu <michaeljota@outlook.com>
2017-08-06 00:26:58 -0400

    feat(start-button): add start-button widget

--- src/app/widgets/index.ts
+++ src/app/widgets/index.ts
@@ -1,7 +1,9 @@
+import { StartButtonWidget } from './start-button/start-button.widget';
 import { TimerProgressWidget } from './timer-progress/timer-progress.widget';
 import { TitleWidget } from './title/title.widget';
 
 export const WIDGETS: ReadonlyArray<any> = [
+  StartButtonWidget,
   TimerProgressWidget,
   TitleWidget,
 ];

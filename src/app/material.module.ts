1bac4ee257b33bcdb838e1eac88f46458aa36d7a
Michael De Abreu <michaeljota@outlook.com>
2017-08-06 00:15:22 -0400

    feat(material): add MdProgressSpinnerModule

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -2,16 +2,19 @@ import { NgModule } from '@angular/core';
 import {
   MdButtonModule,
   MdCardModule,
+  MdProgressSpinnerModule,
 } from '@angular/material';
 
 @NgModule({
   exports: [
     MdButtonModule,
     MdCardModule,
+    MdProgressSpinnerModule,
   ],
   imports: [
     MdButtonModule,
     MdCardModule,
+    MdProgressSpinnerModule,
   ],
 })
 export class MaterialModule {

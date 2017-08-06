f3362df94d6903265134a587f07b06b50a08de60
Michael De Abreu <michaeljota@outlook.com>
2017-08-06 00:15:00 -0400

    feat(material): add MdCardModule

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,14 +1,17 @@
 import { NgModule } from '@angular/core';
 import {
   MdButtonModule,
+  MdCardModule,
 } from '@angular/material';
 
 @NgModule({
   exports: [
     MdButtonModule,
+    MdCardModule,
   ],
   imports: [
     MdButtonModule,
+    MdCardModule,
   ],
 })
 export class MaterialModule {
